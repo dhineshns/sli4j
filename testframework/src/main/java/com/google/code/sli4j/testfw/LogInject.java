dc1c9ba3331d61a8b9c679257ad36c358a275fb9
simone.tripodi <simone.tripodi@d65371b4-0ea7-11df-8d37-15e3504968c6>
2010-02-05 09:19:48 +0000

    first checkin of LogInject class

---
+++ testframework/src/main/java/com/google/code/sli4j/testfw/LogInject.java
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2010 The sli4j Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.google.code.sli4j.testfw;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.google.inject.BindingAnnotation;
+
+/**
+ * Mark a class field as logger that has to be injected.
+ *
+ * @author Simone Tripodi
+ * @version $Id$
+ */
+@Documented
+@BindingAnnotation
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogInject {
+
+}
