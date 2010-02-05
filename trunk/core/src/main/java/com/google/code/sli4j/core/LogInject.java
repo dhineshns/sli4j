05cee9410246679ee9f29cd32c17b5c87a17b5d9
simone.tripodi <simone.tripodi@d65371b4-0ea7-11df-8d37-15e3504968c6>
2010-02-05 09:21:38 +0000

    misplaced @LogInject annotation

--- trunk/core/src/main/java/com/google/code/sli4j/core/LogInject.java
+++ trunk/core/src/main/java/com/google/code/sli4j/core/LogInject.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package com.google.code.sli4j.testfw;
+package com.google.code.sli4j.core;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
