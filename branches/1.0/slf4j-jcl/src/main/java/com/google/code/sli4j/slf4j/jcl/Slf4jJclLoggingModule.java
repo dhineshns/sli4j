ba7410619efb5f0202464171220e9c7542c6d21e
simone.tripodi <simone.tripodi@d65371b4-0ea7-11df-8d37-15e3504968c6>
2010-02-12 08:02:54 +0000

    branched 1.0 version

---
+++ branches/1.0/slf4j-jcl/src/main/java/com/google/code/sli4j/slf4j/jcl/Slf4jJclLoggingModule.java
@@ -0,0 +1,42 @@
+/*
+ *    Copyright 2010 The sli4j Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.google.code.sli4j.slf4j.jcl;
+
+import org.slf4j.Logger;
+
+import com.google.code.sli4j.core.AbstractLoggingModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.matcher.Matcher;
+
+/**
+ * {@code SLF4J - Apache Commons Logging} logger module implementation.
+ *
+ * @author Simone Tripodi
+ * @version $Id$
+ */
+public final class Slf4jJclLoggingModule extends AbstractLoggingModule<Logger> {
+
+    /**
+     * Creates a new {@code SLF4J - Apache Commons Logging} injection module.
+     *
+     * @param matcher types matcher for whom the Logger injection has to be
+     *        performed.
+     */
+    public Slf4jJclLoggingModule(Matcher<? super TypeLiteral<?>> matcher) {
+        super(matcher, Slf4jJclLoggerInjector.class);
+    }
+
+}
---
+++ branches/1.0/slf4j-logback/src/main/java/com/google/code/sli4j/slf4j/logback/Slf4jLogbackLoggerInjector.java
@@ -0,0 +1,56 @@
+/*
+ *    Copyright 2010 The sli4j Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.google.code.sli4j.slf4j.logback;
+
+import java.lang.reflect.Field;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.impl.StaticLoggerBinder;
+
+import com.google.code.sli4j.core.AbstractLoggerInjector;
+
+/**
+ * {@code SLF4J - Logback} logger injector implementation.
+ *
+ * @author Simone Tripodi
+ * @version $Id$
+ */
+public final class Slf4jLogbackLoggerInjector extends AbstractLoggerInjector<Logger> {
+
+    /**
+     * The {@code SLF4J - Logback} logger factory.
+     */
+    private static final ILoggerFactory LOGBACK_LOG_FACTORY = StaticLoggerBinder.getSingleton().getLoggerFactory();
+
+    /**
+     * Creates a new {@code SLF4J - Logback} Logger injector.
+     *
+     * @param field the logger field has to be injected.
+     */
+    public Slf4jLogbackLoggerInjector(Field field) {
+        super(field);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Logger createLogger(Class<?> klass) {
+        return LOGBACK_LOG_FACTORY.getLogger(klass.getName());
+    }
+
+}
