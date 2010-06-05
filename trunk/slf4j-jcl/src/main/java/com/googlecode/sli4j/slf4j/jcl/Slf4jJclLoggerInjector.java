8f8d151ca78d8d4a5fc6280fd2f20fd41da19d8a
simone.tripodi <simone.tripodi@d65371b4-0ea7-11df-8d37-15e3504968c6>
2010-06-05 14:24:28 +0000

    packages migration

--- trunk/slf4j-jcl/src/main/java/com/googlecode/sli4j/slf4j/jcl/Slf4jJclLoggerInjector.java
+++ trunk/slf4j-jcl/src/main/java/com/googlecode/sli4j/slf4j/jcl/Slf4jJclLoggerInjector.java
@@ -13,14 +13,14 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package com.google.code.sli4j.slf4j.jcl;
+package com.googlecode.sli4j.slf4j.jcl;
 
 import java.lang.reflect.Field;
 
 import org.slf4j.Logger;
 import org.slf4j.impl.JCLLoggerFactory;
 
-import com.google.code.sli4j.core.AbstractLoggerInjector;
+import com.googlecode.sli4j.core.AbstractLoggerInjector;
 
 /**
  * {@code SLF4J - Apache Commons Logging} logger injector implementation.
