f960805c2635f8244e826d98eaeee0236739a87e
simone.tripodi <simone.tripodi@d65371b4-0ea7-11df-8d37-15e3504968c6>
2010-06-05 14:31:21 +0000

    packages migration

--- trunk/slf4j-log4j/src/main/java/com/googlecode/sli4j/slf4j/log4j/Slf4jLog4jLoggingModule.java
+++ trunk/slf4j-log4j/src/main/java/com/googlecode/sli4j/slf4j/log4j/Slf4jLog4jLoggingModule.java
@@ -13,13 +13,13 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package com.google.code.sli4j.slf4j.log4j;
+package com.googlecode.sli4j.slf4j.log4j;
 
 import org.slf4j.Logger;
 
-import com.google.code.sli4j.core.AbstractLoggingModule;
 import com.google.inject.TypeLiteral;
 import com.google.inject.matcher.Matcher;
+import com.googlecode.sli4j.core.AbstractLoggingModule;
 
 /**
  * {@code SLF4J - Apache Log4j} logger module implementation.
