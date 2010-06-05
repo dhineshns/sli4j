eab69c0e421972e0959c2b7334ca757ad5b7af20
simone.tripodi <simone.tripodi@d65371b4-0ea7-11df-8d37-15e3504968c6>
2010-06-05 14:06:59 +0000

    packages migration

--- core/src/main/java/com/googlecode/sli4j/core/AbstractLoggerInjector.java
+++ core/src/main/java/com/googlecode/sli4j/core/AbstractLoggerInjector.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package com.google.code.sli4j.core;
+package com.googlecode.sli4j.core;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
